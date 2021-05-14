18aa3a33150232e818252ceca8c989c0d11af518
Joel <joel@deport.me>
2021-05-13 19:54:53 -0500

    Add the ability to hotlink someone to a calculator

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
+window.onload = function() {
+  var pieces = window.location.hash.substr(1).split("/");
+
+  pieces.forEach(function(item, index) {
+    var field = item.split("-");
+    document.getElementById(field[0]).value = field[1];
+  });
+
+  calculate();
+}
+
 function calculate() {
   var dollars   = document.getElementById("dollars").value;
   var time      = document.getElementById("time").value;
   var returns   = document.getElementById("returns").value;
   var inflation = document.getElementById("inflation").value;
+
+  var url = '#';
+  if (dollars) {
+    url += 'dollars-' + dollars + '/';
+  }
+
+  if (time) { 
+    url += 'time-' + time + '/';
+  }
+
+  if (returns) {
+    url += 'returns-' + returns + '/';
+  }
+
+  if (inflation) {
+    url += 'inflation-' + inflation + '/';
+  }
+
+  // Slice is used to strip off trailing '/'
+  if (url != '#') {
+    window.history.pushState({}, '', url.slice(0, -1));
+  }
 
   if (dollars && time && returns && inflation) {
     var yearlySpend = time * dollars;
